6e1954fc18a165f8847b4fbc6f6861438419e085
dotnewt <Antithesis@DESKTOP-E7CE1C0>
2023-02-19 13:05:20 +0500

    Base settings, add Settings Classlib

---
+++ Shared/ComeTogether.Settings/Settings.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ComeTogether.Settings
+{
+    public abstract class Settings
+    {
+        /// <summary>
+        /// Method that loads environment variables from configuration(appsettings.json) by using <paramref name="key"/> to instance of <typeparamref name="T"/> and than returns.
+        /// </summary>
+        public static T? Load<T>(string key, IConfiguration configuration = null)
+        {
+            var instance = (T?)Activator.CreateInstance(typeof(T));
+            SettingsFactory.Create(configuration)
+                .GetSection(key)
+                .Bind(instance, (x) => { x.BindNonPublicProperties = true; });
+
+            return instance;
+        }
+    }
+}
---
+++ Shared/ComeTogether.Settings/SettingsFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ComeTogether.Settings
+{
+    public static class SettingsFactory
+    {
+        /// <summary>
+        /// Creates a configuration file for reading environment variables from {appsettings.json} and {appsettings.Development.json} optional
+        /// </summary>
+        public static IConfiguration Create(IConfiguration? configuration = null)
+        {
+            var conf = configuration ?? new ConfigurationBuilder()
+                                            .SetBasePath(Directory.GetCurrentDirectory())
+                                            .AddJsonFile("appsettings.json", optional: false)
+                                            .AddJsonFile("appsettings.Development.json", optional: true)
+                                            .AddEnvironmentVariables()
+                                            .Build();
+
+            return conf;
+        }
+    }
+}
